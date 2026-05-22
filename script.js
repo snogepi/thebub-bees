e02dd6d427cd5fffec964b4877e6d6fc828cc80d
Christine Joyce Belen <christinejoycebelen@gmail.com>
2026-05-22 22:06:16 +0800

    Remove slash before memories folder

--- script.js
+++ script.js
@@ -2,42 +2,42 @@ const storyPages = [
     {
         pageId: 0,
         milestones: [
-            { id: 1, pathPercent: 0.22, icon: "https://img.icons8.com/?size=100&id=7oDLiKhJyBCV&format=png&color=000000", modalIcon: "/memories/memory001.jpg", date: "march 2024", desc: "first date!" }, // rose
-            { id: 2, pathPercent: 0.52, icon: "https://img.icons8.com/?size=100&id=cwhLAZV7PdD9&format=png&color=000000", modalIcon: "/memories/memory002.jpg", date: "april 2024", desc: "first kiss... and yk" }, // daisy
-            { id: 3, pathPercent: 0.82, icon: "https://img.icons8.com/?size=100&id=wXAKpdSZRGMD&format=png&color=000000", modalIcon: "/memories/memory003.jpg", date: "june 2024", desc: "first... nito" } // tulip
+            { id: 1, pathPercent: 0.22, icon: "https://img.icons8.com/?size=100&id=7oDLiKhJyBCV&format=png&color=000000", modalIcon: "memories/memory001.jpg", date: "march 2024", desc: "first date!" }, // rose
+            { id: 2, pathPercent: 0.52, icon: "https://img.icons8.com/?size=100&id=cwhLAZV7PdD9&format=png&color=000000", modalIcon: "memories/memory002.jpg", date: "april 2024", desc: "first kiss... and yk" }, // daisy
+            { id: 3, pathPercent: 0.82, icon: "https://img.icons8.com/?size=100&id=wXAKpdSZRGMD&format=png&color=000000", modalIcon: "memories/memory003.jpg", date: "june 2024", desc: "first... nito" } // tulip
         ]
     },
     {
         pageId: 1,
         milestones: [
-            { id: 4, pathPercent: 0.25, icon: "https://img.icons8.com/?size=100&id=vgvdUorODfWE&format=png&color=000000", modalIcon: "/memories/memory004.jpg", date: "august 2024", desc: "first birthday salubong" }, // lavender
-            { id: 5, pathPercent: 0.58, icon: "https://img.icons8.com/?size=100&id=fvhMoHEKVJMg&format=png&color=000000", modalIcon: "/memories/memory005.jpg", date: "october 2024", desc: "i don't remember why you were here you probably just missed me" }, // daisy
-            { id: 6, pathPercent: 0.85, icon: "https://img.icons8.com/?size=100&id=gBbOveYifc4w&format=png&color=000000", modalIcon: "/memories/memory006.jpg", date: "december 2024", desc: "1ST CHRISTMAS" } // mistletoe
+            { id: 4, pathPercent: 0.25, icon: "https://img.icons8.com/?size=100&id=vgvdUorODfWE&format=png&color=000000", modalIcon: "memories/memory004.jpg", date: "august 2024", desc: "first birthday salubong" }, // lavender
+            { id: 5, pathPercent: 0.58, icon: "https://img.icons8.com/?size=100&id=fvhMoHEKVJMg&format=png&color=000000", modalIcon: "memories/memory005.jpg", date: "october 2024", desc: "i don't remember why you were here you probably just missed me" }, // daisy
+            { id: 6, pathPercent: 0.85, icon: "https://img.icons8.com/?size=100&id=gBbOveYifc4w&format=png&color=000000", modalIcon: "memories/memory006.jpg", date: "december 2024", desc: "1ST CHRISTMAS" } // mistletoe
         ]
     },
     {
         pageId: 2,
         milestones: [
-            { id: 7, pathPercent: 0.25, icon: "https://img.icons8.com/?size=100&id=HIbj7WorHLRT&format=png&color=000000", modalIcon: "/memories/memory007.jpg", date: "january 2025", desc: "this and gootopia!" }, // sunflower
-            { id: 8, pathPercent: 0.55, icon: "https://img.icons8.com/?size=100&id=T9HN3Xc2RaHJ&format=png&color=000000", modalIcon: "/memories/memory008a.jpg", date: "february 2025", desc: "met pedro and tkim for the first time + tried badminton and baking with bubby" }, // tissue rose
-            { id: 9, pathPercent: 0.85, icon: "https://img.icons8.com/?size=100&id=ZirWHujWhcCH&format=png&color=000000", modalIcon: "/memories/memory009.jpg", date: "may 2025", desc: "tried to recreate our first date... and failed heh" } // carnation
+            { id: 7, pathPercent: 0.25, icon: "https://img.icons8.com/?size=100&id=HIbj7WorHLRT&format=png&color=000000", modalIcon: "memories/memory007.jpg", date: "january 2025", desc: "this and gootopia!" }, // sunflower
+            { id: 8, pathPercent: 0.55, icon: "https://img.icons8.com/?size=100&id=T9HN3Xc2RaHJ&format=png&color=000000", modalIcon: "memories/memory008a.jpg", date: "february 2025", desc: "met pedro and tkim for the first time + tried badminton and baking with bubby" }, // tissue rose
+            { id: 9, pathPercent: 0.85, icon: "https://img.icons8.com/?size=100&id=ZirWHujWhcCH&format=png&color=000000", modalIcon: "memories/memory009.jpg", date: "may 2025", desc: "tried to recreate our first date... and failed heh" } // carnation
         ]
     },
     {
         pageId: 3,
         milestones: [
-            { id: 10, pathPercent: 0.25, icon: "https://img.icons8.com/?size=100&id=tKXB4ORgNHh3&format=png&color=000000", modalIcon: "/memories/memory010.jpg", date: "july 2025", desc: "this after samgyup 🤤" }, // hibiscus
-            { id: 11, pathPercent: 0.55, icon: "https://img.icons8.com/?size=100&id=1pDs7VZLvrqz&format=png&color=000000", modalIcon: "/memories/memory011.jpg", date: "september 2025", desc: "it's just you and me~" }, // blue tulip
-            { id: 12, pathPercent: 0.85, icon: "https://img.icons8.com/?size=100&id=yd4mReUJEvkX&format=png&color=000000", modalIcon: "/memories/memory012.jpg", date: "october 2025", desc: "YOU HAVE STOLEN MY HEART" } // twice bouquet
+            { id: 10, pathPercent: 0.25, icon: "https://img.icons8.com/?size=100&id=tKXB4ORgNHh3&format=png&color=000000", modalIcon: "memories/memory010.jpg", date: "july 2025", desc: "this after samgyup 🤤" }, // hibiscus
+            { id: 11, pathPercent: 0.55, icon: "https://img.icons8.com/?size=100&id=1pDs7VZLvrqz&format=png&color=000000", modalIcon: "memories/memory011.jpg", date: "september 2025", desc: "it's just you and me~" }, // blue tulip
+            { id: 12, pathPercent: 0.85, icon: "https://img.icons8.com/?size=100&id=yd4mReUJEvkX&format=png&color=000000", modalIcon: "memories/memory012.jpg", date: "october 2025", desc: "YOU HAVE STOLEN MY HEART" } // twice bouquet
         ]
     },
     {
         pageId: 4,
         milestones: [
-            { id: 13, pathPercent: 0.20, icon: "https://img.icons8.com/?size=100&id=WYCR35ummeRN&format=png&color=000000", modalIcon: "/memories/memory013.jpg", date: "december 2025", desc: "i have been changed for goodddddd" }, // gardenia
-            { id: 14, pathPercent: 0.42, icon: "https://img.icons8.com/?size=100&id=Pn5GETbGimEC&format=png&color=000000", modalIcon: "/memories/memory014.jpg", date: "february 2025", desc: "don't look at my chin" }, // rose
-            { id: 15, pathPercent: 0.65, icon: "https://img.icons8.com/?size=100&id=Md5CclQNqlvh&format=png&color=000000", modalIcon: "/memories/memory015.jpg", date: "april 2025", desc: "thank you for taking care of me! in every way possible :')" }, // dandelion
-            { id: 16, pathPercent: 0.88, icon: "https://img.icons8.com/?size=100&id=70LKs2EHRJsP&format=png&color=000000", modalIcon: "/memories/birthdaybub.jpg", date: "today", desc: "happy birthday, kyle!" } // hive - kyle pogi
+            { id: 13, pathPercent: 0.20, icon: "https://img.icons8.com/?size=100&id=WYCR35ummeRN&format=png&color=000000", modalIcon: "memories/memory013.jpg", date: "december 2025", desc: "i have been changed for goodddddd" }, // gardenia
+            { id: 14, pathPercent: 0.42, icon: "https://img.icons8.com/?size=100&id=Pn5GETbGimEC&format=png&color=000000", modalIcon: "memories/memory014.jpg", date: "february 2025", desc: "don't look at my chin" }, // rose
+            { id: 15, pathPercent: 0.65, icon: "https://img.icons8.com/?size=100&id=Md5CclQNqlvh&format=png&color=000000", modalIcon: "memories/memory015.jpg", date: "april 2025", desc: "thank you for taking care of me! in every way possible :')" }, // dandelion
+            { id: 16, pathPercent: 0.88, icon: "https://img.icons8.com/?size=100&id=70LKs2EHRJsP&format=png&color=000000", modalIcon: "memories/birthdaybub.jpg", date: "today", desc: "happy birthday, kyle!" } // hive - kyle pogi
         ]
     }
 ];
